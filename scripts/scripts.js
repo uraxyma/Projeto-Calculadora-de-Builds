9e350ea2eebf2294aad43f61092fd09a1a04e217
Bruno R V Silva <dev.brunorvs@gmail.com>
2023-12-13 09:06:34 -0300

    script botão

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,14 @@
 // === variaveis globais
 
 // === === === funcções === === ===
+const buttonCalcular = document.querySelector("#calcular");
+
+buttonCalcular.onclick = () => {
+  const inputNivel = document.querySelector("#nivel");
+  const valorNivel = inputNivel.value;
+
+  const valorRestantes = valorNivel * 3;
+
+  let inputRestantes = document.querySelector("#restantes");
+  inputRestantes.value = valorRestantes;
+};
