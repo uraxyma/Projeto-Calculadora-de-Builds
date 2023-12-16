27f97390420da3b29a73fff1ea32de1bbe1a9543
Bruno R V Silva <dev.brunorvs@gmail.com>
2023-12-16 20:04:12 -0300

    botões de Nível

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,39 @@
-// === variaveis constantes
+// === === === variaveis constantes === === ===
 
-// === variaveis globais
+
+// === === === variaveis globais === === ===
 
 // === === === funcções === === ===
 function calcular() {
-    var nivel = document.getElementById("nivel").value;
-    var resultado = nivel * 3;
-    document.getElementById("atributos").innerHTML = resultado;
+    var nivel = document.querySelector("#nivel").value;
+    var restantes = nivel * 3;
+    document.querySelector("#atributos").innerHTML = restantes;
 }
+
+function addValue(amount) {
+    let inputElement = document.querySelector("#nivel");
+    let currentValue = parseInt(inputElement.value) || 0;
+    let newValue = currentValue + amount;
+
+    newValue = Math.min(75, Math.max(0, newValue));
+
+    inputElement.value = newValue;
+}
+
+function reset() {
+    let inputElement = document.querySelector("#nivel");
+    inputElement.value = 0;
+}
+
+// function validarForca(input) {
+//     const valor = input.value;
+//     const min = input.min;
+//     const max = input.max;
+
+//     if (valor < min || valor > max) {
+//         input.value = "";
+//         return false;
+//     }
+//     return true;
+// }
+
