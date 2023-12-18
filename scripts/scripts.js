9c3f390546981b7b586879d29ed7b15f3fa5baeb
Bruno R V Silva <dev.brunorvs@gmail.com>
2023-12-18 17:20:37 -0300

    scripts for, int

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,22 +2,15 @@
 
 // === === === variaveis globais === === ===
 
-// === === === funcções === === ===
+// === === === funcções abaixo === === ===
+
+// === função calcular ===
 function calcular() {
     var nivel = document.querySelector("#nivel").value;
     var restantes = nivel * 3;
     document.querySelector("#atributos").innerHTML = restantes;
 }
-
-function reset() {
-    // ========= reset Nivel
-    let inputElement = document.querySelector("#nivel");
-    inputElement.value = "00";
-    document.querySelector("#atributos").innerHTML = "000";
-    // ========== Reset Atributos
-
- }
-
+// seção nivel Abaixo
 function addValue(amount) {
     updateValue(amount);
 }
@@ -37,14 +30,70 @@ function updateValue(amount) {
     inputNivel.value = newNivel;
 }
 
-function updateValue(amount) {
-    // ========== função Atributos
-    let inputAtributo = document.querySelector("#forca");
-    let currentAtributo = parseInt(inputAtributo.value) || 1;
-    let newAtributo = currentAtributo + amount;
-
-    newAtributo = Math.min(100, Math.max(1, newAtributo));
-
-    inputAtributo.value = newAtributo;
-    
+function resetNivel() {
+    let inputNivel = document.querySelector("#nivel");
+    inputNivel.value = "00";
+    document.querySelector("#atributos").innerHTML = 00;
+ }
+// seção nivel Acima
+//
+// seção atributos força abaixo <
+function addForca(amount) {
+    updateForca(amount);
 }
+
+function lessForca(amount) {
+    updateForca(amount);
+}
+
+function updateForca(amount) {
+    let inputForca = document.querySelector("#forca");
+    let currentForca = parseInt(inputForca.value) || 1;
+    let newForca = currentForca + amount;
+
+    newForca = Math.min(100, Math.max(1, newForca));
+
+    inputForca.value = newForca;
+}
+
+function resetForca() {
+    let inputForca = document.querySelector("#forca");
+    inputForca.value = 001;
+    document.querySelector("#forca").innerHTML = 001;
+}
+// seção atributos força acima >
+//
+// seção atributos inteligencia abaixo <
+
+function addInt(amount) {
+    updateInt(amount);
+}
+
+function lessInt(amount) {
+    updateInt(amount);
+}
+
+function updateInt(amount) {
+    let input = document.querySelector("#int");
+    let current = parseInt(input.value) || 1;
+    let newInt = current + amount;
+
+    newInt = Math.min(100, Math.max(1, newInt));
+
+    input.value = newInt;
+}
+
+function resetInt() {
+    let input = document.querySelector("#int");
+    input.value = 001;
+    document.querySelector("#int").innerHTML = 001;
+}
+
+// seção atributos inteligencia acima >
+//
+// seção atributos Destreza Abaixo <
+
+
+
+// seção atributos Destreza acima >
+//
