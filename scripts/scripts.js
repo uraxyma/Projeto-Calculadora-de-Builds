6e98b8c76c18423e04bb4acd614a8adf09aee1a7
Bruno R V Silva <dev.brunorvs@gmail.com>
2023-12-20 08:38:56 -0300

    implementou todos os botões

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -33,8 +33,8 @@ function updateValue(amount) {
 function resetNivel() {
     let inputNivel = document.querySelector("#nivel");
     inputNivel.value = "00";
-    document.querySelector("#atributos").innerHTML = 00;
- }
+    document.querySelector("#atributos").innerHTML = 0;
+}
 // seção nivel Acima
 //
 // seção atributos força abaixo <
@@ -58,8 +58,8 @@ function updateForca(amount) {
 
 function resetForca() {
     let inputForca = document.querySelector("#forca");
-    inputForca.value = 001;
-    document.querySelector("#forca").innerHTML = 001;
+    inputForca.value = 1;
+    document.querySelector("#forca").innerHTML = 1;
 }
 // seção atributos força acima >
 //
@@ -85,15 +85,208 @@ function updateInt(amount) {
 
 function resetInt() {
     let input = document.querySelector("#int");
-    input.value = 001;
-    document.querySelector("#int").innerHTML = 001;
+    input.value = 1;
+    document.querySelector("#int").innerHTML = 1;
 }
 
 // seção atributos inteligencia acima >
 //
 // seção atributos Destreza Abaixo <
+function addDes(amount) {
+    updateDes(amount);
+}
 
+function lessDes(amount) {
+    updateDes(-amount);
+}
 
+function updateDes(amount) {
+    let input = document.querySelector("#des");
+    let current = parseInt(input.value) || 1;
+    let newDes = current + amount;
 
+    newDes = Math.min(100, Math.max(1, newDes));
+
+    input.value = newDes;
+}
+
+function resetDes() {
+    let input = document.querySelector("#des");
+    input.value = 1;
+    document.querySelector("#des").innerHTML = 1;
+}
 // seção atributos Destreza acima >
 //
+// seção atributos agilidade abaixo <
+function addAgi(amount) {
+    updateAgi(amount);
+}
+
+function lessAgi(amount) {
+    updateAgi(-amount);
+}
+
+function updateAgi(amount) {
+    let input = document.querySelector("#agi");
+    let current = parseInt(input.value) || 1;
+    let newAgi = current + amount;
+
+    newAgi = Math.min(100, Math.max(1, newAgi));
+
+    input.value = newAgi;
+}
+
+function resetAgi() {
+    let input = document.querySelector("#agi");
+    input.value = 1;
+    document.querySelector("#agi").innerHTML = 1;
+}
+// seção atributos agilidade acima >
+//
+// seção atributos vigor abaixo <
+function addVig(amount) {
+    updateVig(amount);
+}
+
+function lessVig(amount) {
+    updateVig(-amount);
+}
+
+function updateVig(amount) {
+    let input = document.querySelector("#vig");
+    let current = parseInt(input.value) || 1;
+    let newVig = current + amount;
+
+    newVig = Math.min(100, Math.max(1, newVig));
+
+    input.value = newVig;
+}
+
+function resetVig() {
+    let input = document.querySelector("#vig");
+    input.value = 1;
+    document.querySelector("#vig").innerHTML = 1;
+}
+// seção atributos vigor acima >
+//
+// seção atributos Tal abaixo <
+function addTal(amount) {
+    updateTal(amount);
+}
+
+function LessTal(amount) {
+    updateTal(-amount);
+}
+
+function updateTal(amount) {
+    let input = document.querySelector("#tal");
+    let current = parseInt(input.value) || 1;
+    let newTal = current + amount;
+
+    newTal = Math.min(100, Math.max(1, newTal));
+
+    input.value = newTal;
+}
+
+function resetTal() {
+    let input = document.querySelector("#tal");
+    input.value = 1;
+    document.querySelector("#tal").innerHTML = 1;
+}
+// seção atributos sorte acima >
+//
+// seção atributos sorte abaixo <
+function addSor(amount) {
+    let input = document.querySelector("#sor");
+    let current = parseInt(input.value) || 1;
+    let newSor = current + amount;
+
+    newSor = Math.min(100, Math.max(1, newSor));
+
+    input.value = newSor;
+}
+
+function resetSor() {
+    let input = document.querySelector("#sor");
+    input.value = 1;
+    document.querySelector("#sor").innerHTML = 1;
+}
+// seção atributos sorte acima >
+//
+// seção atributos fé abaix <
+function addFe(amount) {
+    updateFe(amount);
+}
+
+function lessFe(amount) {
+    updateFe(-amount);
+}
+
+function updateFe(amount) {
+    let input = document.querySelector("#fe");
+    let current = parseInt(input.value) || 1;
+    let newFe = current + amount;
+
+    newFe = Math.min(100, Math.max(1, newFe));
+
+    input.value = newFe
+}
+
+function resetFe() {
+    let input = document.querySelector("#fe");
+    input.value = 1;
+    document.querySelector("#fe").innerHTML = 1;
+}
+// seção atributos Fé acima >
+//
+// Seção ESTATUS do personagem </>
+
+// Função vida
+
+// Função Estamina
+
+// Função Mana
+
+// Função Taxa de critico
+
+// Função Dano Critico
+
+// Função Armadura
+
+// Função Acerto
+
+// Função Esquiva
+
+// Seção RESISTENCIA MARCIAIS </>
+
+// Função cortante
+
+// Função Perfurante
+
+// Função Contundente
+
+// RESISTENCIAS ARCANAS
+
+// Função chaos
+
+// função radiante
+
+// função psiquico
+
+// função fantasma
+
+// função explosão
+
+// RESISTENCIAS NATURAIS
+
+// função Geoturgo / Mineraturgo
+
+// função hidromante / criomante
+
+// função aeroturgo / eletroturgo
+
+// função piromante
+
+// função poison
+
+// CALCULADORA DE DANOS
